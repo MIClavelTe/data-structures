2234406a7c397ff816d248052fbe019dd797db17
MIClavelTe <miclavelte@gmail.com>
2019-08-05 12:49:17 -0700

    Create base case and recursive action

---
+++ merge_sort.py
@@ -0,0 +1,10 @@
+def merge_sort (list):
+    if len(list) <= 1:
+        return list
+    
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
