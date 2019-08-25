c8e3bfe75aa170941bfce41fd4f20ccec02827c9
MIClavelTe <miclavelte@gmail.com>
2019-08-25 14:14:21 -0700

    Create split function and node_at_index function

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -7,3 +7,26 @@ def merge_sort(linked_list):
         return linked_list
 
     left_half, right_half = split(linked_list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+def split(linked_list):
+    if linked_list == None or linked_list.head == None:
+        left_half = linked_list
+        right_half = None
+
+        return left_half, right_half
+    else:
+        size = linked_list.size()
+        mid = size//2
+
+        mid_node = linked_list.node_at_index(mid-1)
+    
+        left_half = linked_list
+        right_half = LinkedList()
+        right_half.head = mid_node.next_node
+        mid_node.next_node = None
+
+        return left_half, right_half
